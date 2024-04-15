270737b94abd15e8aacd187f0d69972e074e9ac3
Zane41 <korener@rambler.ru>
2024-04-15 11:45:14 +0300

    задание 3 и 4

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        calcAverPerimTask2(task1());
+        calcSumSquare(task3(new File(("INPUT.txt"))));
 
     }
 
 
-    public List<Triangle> task1 () {
+    public static ArrayList<Triangle> task1() {
         System.out.println("Введи координаты 6 точек");
         Scanner vvod = new Scanner(System.in);
         ArrayList<Triangle> returnList = new ArrayList<>();
@@ -27,10 +28,31 @@ public class Main {
         }
             return returnList;
     }
-    public double calcAverPerim (ArrayList<Triangle> list){
+    public static double calcAverPerimTask2 (ArrayList<Triangle> list){
         double per = 0;
         for(Triangle x:list)
           per += x.calcPerim(x) ;
-        per= per/list.size(); return per;
+        per= per/list.size();
+        System.out.println("Средний периметр треугольников равен: "+per);
+        return per;
+    }
+    public static ArrayList<Triangle> task3 (File file) throws FileNotFoundException {
+        Scanner vvod = new Scanner(file);
+        ArrayList<Triangle> returnList = new ArrayList<>();
+        while (vvod.hasNextLine()) {
+            returnList.add(new Triangle(new Point(vvod.nextDouble(), vvod.nextDouble()),
+                    new Point(vvod.nextDouble(), vvod.nextDouble()),
+                    new Point(vvod.nextDouble(), vvod.nextDouble())));
+        vvod.close();
+        }
+        return returnList;
+    }
+    public static void calcSumSquare (ArrayList<Triangle> list){
+        double sumSq = 0;
+        for (Triangle x:list)
+            sumSq+=Math.sqrt(x.calcPerim(x)*(x.calcPerim(x)-Point.dlinaOtrezka(x.getPointA(),x.getPointB()))*
+                    (x.calcPerim(x)-Point.dlinaOtrezka(x.getPointB(),x.getPointC()))*
+                    (x.calcPerim(x)-Point.dlinaOtrezka(x.getPointC(),x.getPointA())));
+        System.out.println("Суммарная площадь треугольников равна: "+sumSq);
     }
 }
